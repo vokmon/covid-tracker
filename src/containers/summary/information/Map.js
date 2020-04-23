ea758951705065630b8bf17e390f7e17352e1391
Arnon Ruangthanawes <arnon@precisionsolutions.co.nz>
2020-04-23 12:51:14 +0700

    Add map

--- src/containers/summary/information/Map.js
+++ src/containers/summary/information/Map.js
@@ -2,17 +2,12 @@ import React from 'react';
 import {
   Segment,
 } from 'semantic-ui-react';
-import { useTranslation } from 'react-i18next';
 import style from './Map.module.scss';
 
-const Map = () => {
-  const { t } = useTranslation();
-
-  return (
-    <Segment className={style.container}>
-      <iframe className={style.iframe} src='https://covid19.th-stat.com/th/share/map' title='map' />
-    </Segment>
-  );
-};
+const Map = () => (
+  <Segment className={style.container}>
+    <iframe className={style.iframe} src='https://covid19.th-stat.com/th/share/map' title='map' />
+  </Segment>
+);
 
 export default Map;
